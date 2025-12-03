10a916db01b3dc03b7b49b7e8a3bee18570388fa
arthursiles <carlos.silles@gmail.com>
2025-12-02 23:14:19 -0300

    última atualização

---
+++ index.js
@@ -0,0 +1,29 @@
+let nome = "Hendrix"
+let xp = 123
+    
+
+if(xp <= 1000){
+    console.log("O herói " + nome + " está no nível Ferro")
+}
+else if(xp >= 1001 && xp <= 2000){
+    console.log("O herói " + nome + " está no nível Bronze")
+}
+else if(xp >= 20017 && xp <= 5000){
+    console.log("O herói " + nome + " está no nível Prata ")
+}
+else if(xp >= 5001 && xp <= 7000){
+    console.log("O herói " + nome + " está no nível Ouro")
+}
+else if(xp >= 7000 && xp <= 8000){
+    console.log("O herói " + nome + " está no nível Platina")
+}
+else if(xp >= 8001 && xp <= 9000){
+    console.log("O herói " + nome + " está no nível Ascendente")
+}
+else if(xp >= 9001 && xp <= 10000){
+    console.log("O herói " + nome + " está no nível Imortal")
+}
+else{
+    console.log("O herói " + nome + " está no nível Radiante")
+}
+
